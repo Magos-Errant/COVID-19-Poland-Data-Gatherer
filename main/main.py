e3683d24d116f4e2f6c0d3c58f55315ee90d159c
Magos-Errant <qbit666@gmail.com>
2020-12-03 23:35:32 +0900

    Signed-off-by: Magos-Errant <qbit666@gmail.com>

--- main/main.py
+++ main/main.py
@@ -1,33 +1,33 @@
 from unidecode import unidecode
 import re
 
-date = "30/11/2020"
+target_day = 30
+target_month = 11
+target_year = 20
 
-target_day = date[:2]
-target_month = date[3:5]
-target_year = date[-2:]
+
+
+
 
 def DataFormatter(target_day, target_month, target_year):
     file_location = f"Y:\Fukuoka Research\Covid-19 polska\\{target_day}_{target_month}_{target_year}_wojewodztwa.csv"
-    file_output = f"Y:\Fukuoka Research\Covid-19 polska\\{target_day}_{target_month}_{target_year}_wojewodztwa_normalized.csv"
-
+    file_output = f"Y:\Fukuoka Research\Covid-19 polska\\All_wojewodztwa_normalized.csv"
 
     data_bucket = open(file_location, 'r')
     current_file = data_bucket.readlines()
     data_bucket.close()
 
     current_file = [line.strip() for line in current_file]
+    current_file = current_file[1:]
+    save = open(file_output, 'a+')
 
-    save = open(file_output, 'w')
 
 
     d = {'œ': 's', '³': 'l', '¹': 'a', ',': '.', ';': ','}
     for line in current_file:
+
         for key in d.keys():
             line = line.replace(key, d[key])
-        change_id = re.search('id', line)
-        if change_id:
-            line = line[:-2] + "Date"
 
         test = re.search('(t[0-9]+)', line)
         if test:
@@ -37,6 +37,11 @@ def DataFormatter(target_day, target_month, target_year):
 
     save.close()
 
-DataFormatter(target_day, target_month, target_year)
+
+
+for i in range(24,target_day+1):
+    date = str(f"{i}/{target_month}/20{target_year}")
+    print(i)
+    DataFormatter(i, target_month, target_year)
 
 
